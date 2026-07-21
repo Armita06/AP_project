2e5291dae5ba21ef1488d02e953c2c1f0754e209
zzz <zzzey.rr@gmail.com>
2026-07-21 06:29:59 +0330

    Upload my clean project code

---
+++ src/main/java/server/Application.java
@@ -0,0 +1,17 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"model"})
+@EnableJpaRepositories(basePackages = {"repository"})
+@ComponentScan(basePackages = {"server", "service", "controller"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
